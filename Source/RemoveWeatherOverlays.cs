6e3b67a60699a1baf4cb5013ef276b7d3c90e077
Morgan V <mvaraine@gmail.com>
2021-05-03 18:38:44 +1200

    Use Harmony Patcher to patch Verse.WeatherWorker.DrawWeather() Remove redundant files

--- Source/RemoveWeatherOverlays.cs
+++ Source/RemoveWeatherOverlays.cs
@@ -1,8 +1,37 @@
-﻿using System;
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
 
-namespace RemoveWeatherOverlays
+
+namespace yipnz.RemoveWeatherOverlays
 {
-    public class RemoveWeatherOverlays
+    [StaticConstructorOnStartup]
+    public static class OverlayPatcher
     {
+
+        public static void DoOverlayPatcher()
+        {
+            Log.Message("RemoveWeatherOverlays - Patching Verse.WeatherWorker.DrawOverlays()");
+            var instance = new Harmony("yipnz.RWO");
+            instance.PatchAll();
+        }
+
+    }
+
+    class DrawOverlayPatch
+    {
+        // skip autosave
+		[HarmonyPatch(typeof(WeatherWorker), nameof(WeatherWorker.DrawWeather))]
+		public class WeatherWorker_DrawWeather_Patch
+		{
+			public static bool Prefix()
+			{
+				return false;
+			}
+		}
+
     }
 }
